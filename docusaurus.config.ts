9b8b9ce491c9d584380e83be576e293b14670ce9
Nitin Kumar <snitin315@gmail.com>
2025-12-25 15:20:07 +0530

    fix: update settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'marshmallo-ai', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
